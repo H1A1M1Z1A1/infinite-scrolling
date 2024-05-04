33568cf5e72cc6c2c7f9071fee335d00a9c1f911
HAMZA <92072539+H1A1M1Z1A1@users.noreply.github.com>
2024-05-04 21:23:02 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useContext } from "react";
+import Home from "./component3/Home";
+import Array from "./component3/Array";
+
+const App1 = () => {
+  // const {Transactions}=useContext();
+  return (
+    <div>
+      <div>
+        <Home></Home>
+        {/* <Array></Array> */}
+      </div>
+    </div>
+  );
+};
+
+export default App1;
